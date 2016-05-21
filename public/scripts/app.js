b9d1056a1c90e298d2790c6b8b0b4b406777f3ef
Jake Priddy <priddyjacob84@gmail.com>
2016-05-21 13:44:33 -0400

    5/21 1344

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -1,24 +1,22 @@
-<script>
+
   // Initialize Firebase
   var Firebase = require("firebase");
-  var config = {
+var config = {
     apiKey: "AIzaSyB_6B4i_JJe8cxhXZv8vIBZgwRoRK_FObA",
     authDomain: "jchat-18544.firebaseapp.com",
     databaseURL: "https://jchat-18544.firebaseio.com",
     storageBucket: "jchat-18544.appspot.com",
-  };
+};
   firebase.initializeApp(config);
-</script>
-    <script src="https://www.gstatic.com/firebasejs/live/3.0/firebase.js"></script>
-<script>
-    var lblCurrentMessage = document.getElementById('lblCurrentMessage'),
-        txtNewMessage = document.getElementById('txtNewMessage'),
-        btUpdateMessage = document.getElementById('btUpdateMessage');
-        rootRef = new Firebase('https://jchat-18544.firebaseapp.com/'),
-        currentMessageRef = rootRef.child('currentMessage');
 
-            btUpdateMessage.addEventListener('click', function() {
-              currentMessageRef.set(txtNewMessage.value);
-                txtNewMessage.value='';
-            });
-</script>
+var lblCurrentMessage = document.getElementById('lblCurrentMessage'),
+    txtNewMessage = document.getElementById('txtNewMessage'),
+    btUpdateMessage = document.getElementById('btUpdateMessage'),
+    login = document.getElementById('login'),
+    rootRef = firebase.database(),
+    currentMessageRef = rootRef.ref('currentMessage');
+
+btUpdateMessage.addEventListener('click', function() {
+    currentMessageRef.set(txtNewMessage.value);
+    txtNewMessage.value='';
+});
