7d83819be7866886cadcee1b51b6a389d5e49652
Jake Priddy <priddyjacob84@gmail.com>
2016-05-22 11:40:02 -0400

    5/22 1139

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -8,7 +8,29 @@ var config = {
     storageBucket: "jchat-18544.appspot.com",
 };
   firebase.initializeApp(config);
-
+//-----------auth signIn
+login.addEventListener('click', function() { firebase.auth().signInWithPopup(provider).then(function(result) {
+  // This gives you a Google Access Token. You can use it to access the Google API.
+  var token = result.credential.accessToken;
+  // The signed-in user info.
+  var user = result.user;
+  // ...
+}).catch(function(error) {
+  // Handle Errors here.
+  var errorCode = error.code;
+  var errorMessage = error.message;
+  // The email of the user's account used.
+  var email = error.email;
+  // The firebase.auth.AuthCredential type that was used.
+  var credential = error.credential;
+  // ...
+});
+//-----------auth signOut
+firebase.auth().signOut().then(function() {
+  // Sign-out successful.
+}, function(error) {
+  // An error happened.
+});
 var lblCurrentMessage = document.getElementById('lblCurrentMessage'),
     txtNewMessage = document.getElementById('txtNewMessage'),
     btUpdateMessage = document.getElementById('btUpdateMessage'),
