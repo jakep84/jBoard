00093a3cd218e674586182d39b906a848f82d385
Jake Priddy <priddyjacob84@gmail.com>
2016-05-23 00:34:22 -0400

    5/23 0034

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -1,96 +1,30 @@
 
 // Initialize Firebase
-//var firebase = require("firebase");
+var Firebase = require("firebase");
 var config = {
     apiKey: "AIzaSyB_6B4i_JJe8cxhXZv8vIBZgwRoRK_FObA",
     authDomain: "jchat-18544.firebaseapp.com",
     databaseURL: "https://jchat-18544.firebaseio.com",
-    storageBucket: "jchat-18544.appspot.com",
+    storageBucket: "jchat-18544.appspot.com"
 };
-firebase.initializeApp(config);
+Firebase.initializeApp(config);
 //-----------auth signIn
-var auth = firebase.auth();
-var database = firebase.database();
-var provider = new firebase.auth.GoogleAuthProvider();
-
-$(document).ready(function() { 
-    if (jApp.isLoggedIn()) {
-        $('#loginInfo').html(jApp,currentUser.google.displayName);
-        $('#loggedIn').show();
-        $('#btnLogin').hide();
-        messageClass.getMessages();
-    } else {
-        $('#loggedIn').hide();
-        $('#btnLogIn').show();
-    }
-    $('#btnLogin').on('click',jApp.login);
-    $('#btnLogout').on('click',jApp.logout);
-    $('#btUpdateMessage').on('click',messageClass.postMessage);
-})
-var jApp = {
-    currentUser : {},
-    username: '',
-    isLoggedIn: function () {
-        jApp.currentUser = auth.currentUser;
-        if (jApp.currentUser !== null) {
-            jApp.username = jApp.currentUser.displayName;
-        }
-        return jApp.currentUser !== null;
-    }, 
-    login: function() {
-        if (!jApp.isLoggedIn()) {
-            auth.signInWithPopup (provider).then(function(result) {
-
-                login.addEventListener('click', function() {
-                    //a sign in popup will be generated
-                    //I will use popup, redirect is preffered in mobile though
-                    firebase.auth().signInWithPopup(provider).then(function(result) {
-                        // This gives you a Google Access Token. You can use it to access the Google API.
-                        var token = result.credential.accessToken;
-                        // The signed-in user info.
-                        user = result.user;
-                        username = jApp.currentUser.displayName;
-                        $('#loginInfo').html(username);
-                        $('#loggedIn').show();
-                        $('#btnLogin').hide();
-                        messageClass.getMessages();
-                    }).catch(function(error) {
-                        // Handle Errors here.
-                        var errorCode = error.code;
-                        var errorMessage = error.message;
-                        // The email of the user's account used.
-                        var email = error.email;
-                        // The firebase.auth.AuthCredential type that was used.
-                        var credential = error.credential;
-                        $('#loginInfo').html(error.message);
-                    } else {
-                $('#loginInfo').html(jApp.username);
-                $('#loginInfo').html(jApp.username);                   
-                $('#loggedIn').show();                   
-                $('#btnLogin').hide(); 
-                messageClass.getMessages();
-            })
-            });
-            logout: function(){
-                auth.signOut().then(function () {
-                    jApp.currentUser = null;
-                    jApp.username = '';
-                    $('#loggedIn').hide();
-                    $('#btnLogin').show();
-                }, function(error) {
-                    $('#loginInfo').html(error.message);
-                    // An error happened.
-                });
-                var messageClass = (function() {
+var auth = Firebase.auth();
+var database = Firebase.database();
+var provider = new Firebase.auth.GoogleAuthProvider();
+var messageClass = (function() {
                     var postMessage = function(event) {
                         //to keep prevent a DOM reload of js on refresh
-                        event.preventDefault()
+                        event.preventDefault();
                         //grab user input message
                         var message = $('#message').val();
                         //a section for messages in my database
+                        var messagesReference = database.ref('messages');
+
                         $('#message').val('');
                         //clear message section to aknowledge reciept
-                        var messageReference.push({
+                        //save data to the database using the set method
+                            messageReference.push({
                             message: message,
                             user:jApp.username
                         })
@@ -126,8 +60,78 @@ var jApp = {
                                   getMessages: getMessages,
                                   update<essage: updateMessage
                                   };
-                })}};
-                
+                });
+var jApp = {
+    currentUser : {},
+    username: '',
+    isLoggedIn: function () {
+        jApp.currentUser = auth.currentUser;
+        if (jApp.currentUser !== null) {
+            jApp.username = jApp.currentUser.displayName;
+        }
+        return jApp.currentUser !== null;
+    }, 
+    login: function() {
+        if (!jApp.isLoggedIn()) {
+            auth.signInWithPopup(provider).then(function(result) {
+
+                login.addEventListener('click', function() {
+                    //a sign in popup will be generated
+                    //I will use popup, redirect is preffered in mobile though
+                    Firebase.auth().signInWithPopup(provider).then(function(result) {
+                        // This gives you a Google Access Token. You can use it to access the Google API.
+                        var token = result.credential.accessToken;
+                        // The signed-in user info.
+                        var user = result.user;
+                        username = jApp.currentUser.displayName;
+                        $('#loginInfo').html(username);
+                        $('#loggedIn').show();
+                        $('#btnLogin').hide();
+                        messageClass.getMessages();
+                    }).catch(function(error) {
+                        // Handle Errors here.
+                        var errorCode = error.code;
+                        var errorMessage = error.message;
+                        // The email of the user's account used.
+                        var email = error.email;
+                        // The firebase.auth.AuthCredential type that was used.
+                        var credential = error.credential;
+                        $('#loginInfo').html(error.message);
+                    } else {
+                $('#loginInfo').html(jApp.username);
+                $('#loginInfo').html(jApp.username);                   
+                $('#loggedIn').show();                   
+                $('#btnLogin').hide(); 
+                messageClass.getMessages();
+            })
+        }),
+            logout: function(){
+                auth.signOut().then(function () {
+                    jApp.currentUser = null;
+                    jApp.username = '';
+                    $('#loggedIn').hide();
+                    $('#btnLogin').show();
+                }, function(error) {
+                    $('#loginInfo').html(error.message);
+                    // An error happened.
+                });
+                }; })
+    }};
+};
+    $(document).ready(function (){ 
+    if (jApp.isLoggedIn()) {
+        $('#loginInfo').html(jApp,currentUser.google.displayName);
+        $('#loggedIn').show();
+        $('#btnLogin').hide();
+        messageClass.getMessages();
+    } else {
+        $('#loggedIn').hide();
+        $('#btnLogIn').show();
+    }
+    $('#btnLogin').on('click',jApp.login);
+    $('#btnLogout').on('click',jApp.logout);
+    $('#btUpdateMessage').on('click',messageClass.postMessage);
+});
                                   
                                   
                                   
