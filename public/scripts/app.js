288afb2fac7b417762299b6063f1c34049401f7b
Jake Priddy <priddyjacob84@gmail.com>
2016-05-25 01:29:54 -0400

    5/25 0129

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -1,12 +1,19 @@
 // Initialize Firebase
-  var config = {
-    apiKey: "AIzaSyB_6B4i_JJe8cxhXZv8vIBZgwRoRK_FObA",
-    authDomain: "jchat-18544.firebaseapp.com",
-    databaseURL: "https://jchat-18544.firebaseio.com/",
-    storageBucket: "jchat-18544.appspot.com",
-  };
-  firebase.initializeApp(config);
-
+//  var config = {
+//    apiKey: "AIzaSyB_6B4i_JJe8cxhXZv8vIBZgwRoRK_FObA",
+//    authDomain: "jchat-18544.firebaseapp.com",
+//    databaseURL: "https://jchat-18544.firebaseio.com/",
+//    storageBucket: "jchat-18544.appspot.com",
+//  };
+//  firebase.initializeApp(config);
+var firebase = require("firebase");
+firebase.initializeApp({
+  apiKey: "AIzaSyB_6B4i_JJe8cxhXZv8vIBZgwRoRK_FObA",
+  authDomain: "jchat-18544.firebaseapp.com",
+  serviceAccount: "/package.json",
+  databaseURL: "https://jchat-18544.firebaseio.com/",
+  storageBucket: "jchat-18544.appspot.com"
+});
 //-----------auth signIn
 var auth = firebase.auth();
 var provider = new firebase.auth.GoogleAuthProvider();
